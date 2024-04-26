98ddd0de168dc758ffbd4546a4307b4841bf0215
Ali Salehi <nysalehi@gmail.com>
2024-04-26 15:03:22 +0200

    Add sender.

---
+++ source/sender/sender.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <span>
+
+#include <network/udp_server.h>
+
+namespace Tyr::Sender
+{
+class Sender
+{
+private:
+    std::shared_ptr<Common::UdpServer> commUDP;
+
+public:
+    int buff_idx;
+    int startup;
+
+    void getCommand(std::span<unsigned char> data);
+    bool appendData(unsigned char *data, int length);
+    bool sendAll();
+    void append_demo_data();
+
+    Sender();
+};
+} // namespace Tyr::Sender
