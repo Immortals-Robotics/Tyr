11c4bce55d5e3c7d9b0038f34a2bc1351d5b9b50
Ali Salehi <nysalehi@gmail.com>
2024-04-26 14:46:36 +0200

    Add random to common.

---
+++ source/common/math/random.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <random>
+
+namespace Tyr::Common
+{
+class Random
+{
+private:
+    std::random_device                    *rnd_device;
+    std::mt19937                          *rnd_engine;
+    std::uniform_real_distribution<float> *rnd_dist;
+
+    float min;
+    float max;
+
+public:
+    Random();
+    Random(float _min, float _max);
+    ~Random();
+
+    float get() const;
+};
+} // namespace Tyr::Common
