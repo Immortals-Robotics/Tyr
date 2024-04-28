dda4b2d64a6f05fc22c1b1471a6ed33e90079a5d
Ali Salehi <nysalehi@gmail.com>
2024-04-28 18:04:21 +0200

    Add the remaining soccer code.

---
+++ source/soccer/helpers/strategy_weight.cpp
@@ -0,0 +1,56 @@
+#include "../ai.h"
+
+namespace Tyr::Soccer
+{
+int Ai::strategy_weight(void)
+{
+    if (!playBook)
+    {
+        std::cout << "strategy_weight__+:  444" << std::endl;
+        return -1;
+    }
+
+    if (playBook->strategy_size() == 0)
+    {
+        std::cout << "strategy_weight__+:  555" << std::endl;
+        return -1;
+    }
+    std::cout << "strategy_weight__+:  333" << std::endl;
+
+    std::map<int, float> good_strs;
+
+    float sigma_w = 0;
+
+    std::cout << "	strategy:	";
+    for (int i = 0; i < playBook->strategy_size(); i++)
+    {
+        if ((side * ball.Position.x > playBook->strategy(i).minx()) &&
+            (side * ball.Position.x < playBook->strategy(i).maxx()) &&
+            (std::fabs(ball.Position.y) > playBook->strategy(i).miny()) &&
+            (std::fabs(ball.Position.y) < playBook->strategy(i).maxy()) && (playBook->weight(i) > 0))
+        {
+            std::cout << i << "|" << playBook->weight(i) << "	";
+            good_strs[i] = sigma_w + playBook->weight(i);
+            sigma_w += playBook->weight(i);
+        }
+    }
+    std::cout << std::endl;
+
+    if (sigma_w == 0)
+    {
+        return -1;
+    }
+
+    int ans_str = -1;
+    for (std::map<int, float>::const_iterator it = good_strs.begin(); it != good_strs.end(); ++it)
+    {
+        if (randomParam * sigma_w <= it->second)
+        {
+            ans_str = it->first;
+            break;
+        }
+    }
+
+    return ans_str;
+}
+} // namespace Tyr::Soccer
