a38c22c180885295c12183491ea97fa89304bf06
Ali Salehi <nysalehi@gmail.com>
2024-04-29 00:32:35 +0200

    Add line segment.

---
+++ source/common/math/geom/line_segment.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../vector.h"
+
+namespace Tyr::Common
+{
+struct LineSegment
+{
+    Common::Vec2 start;
+    Common::Vec2 end;
+};
+} // namespace Tyr::Common
--- source/common/math/geom/rect.h
+++ source/common/math/geom/rect.h
@@ -7,12 +7,9 @@ struct Rect
 {
 public:
     Rect(const Vec2 t_p1, const Vec2 t_p2)
-    {
-        min_x = std::min(t_p1.x, t_p2.x);
-        min_y = std::min(t_p1.y, t_p2.y);
-        max_x = std::max(t_p1.x, t_p2.x);
-        max_y = std::max(t_p1.y, t_p2.y);
-    }
+        : min(std::min(t_p1.x, t_p2.x), std::min(t_p1.y, t_p2.y)),
+          max(std::max(t_p1.x, t_p2.x), std::max(t_p1.y, t_p2.y))
+    {}
 
     bool inside(Vec2 t_point) const;
 
@@ -22,9 +19,7 @@ public:
 
     bool isCircleCross(Vec2 t_point1, Vec2 t_point2, float t_r) const;
 
-    float min_x = 0.0;
-    float min_y = 0.0;
-    float max_x = 0.0;
-    float max_y = 0.0;
+    Vec2 min;
+    Vec2 max;
 };
 } // namespace Tyr::Common
