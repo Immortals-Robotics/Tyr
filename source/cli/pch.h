38a74c5841accfb9465c9256cc79286552fdc8bf
Ali Salehi <nysalehi@gmail.com>
2024-05-01 11:14:27 +0200

    Add cli project.

---
+++ source/cli/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include <ai.h>
+#include <grsim.h>
+#include <referee.h>
+#include <sender.h>
+#include <services.h>
+#include <vision.h>
