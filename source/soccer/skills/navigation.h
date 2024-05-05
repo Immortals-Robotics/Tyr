476fc3a96bfa43025507debd4c7c8263fd1e031a
Ali Salehi <nysalehi@gmail.com>
2024-05-05 08:24:34 +0200

    Experimenting with navigation.

--- source/soccer/skills/navigation.h
+++ source/soccer/skills/navigation.h
@@ -1,24 +1,40 @@
 #pragma once
 
-#include "skill.h"
+#include "../dss/dss.h"
+#include "../errt/errt.h"
+#include "../obstacle/obstacle.h"
 
 namespace Tyr::Soccer::Skill
 {
 
-class Navigation final : public ISkill
+class Navigation
 {
 public:
     struct Params
     {
-        Common::Vec2     dest;
-        int              speed;
-        VelocityProfile *velocityProfile;
-        bool             use_dss;
+        Common::Vec2 dest;
+
+        int             speed = 80;
+        VelocityProfile velocity_profile;
+
+        bool use_errt = true;
+        bool use_dss  = true;
+
+        bool obs_ball  = false;
+        bool obs_field = true;
     };
 
-    void setParams(const Params &t_params);
+    void execute(Robot *t_robot, const Params &t_params);
 
-    void execute(Robot *t_robot) override;
+private:
+    void setObstacles(Robot *t_robot, const Params &t_params);
+
+    ObsMap m_obs_map;
+
+    Planner m_planner;
+    Dss     m_dss;
+
+    bool m_navigated;
 };
 
 } // namespace Tyr::Soccer::Skill
