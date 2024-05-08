01f111bdba1d26cab5cfdfe555e1ea6ccbbbac11
Ali Salehi <nysalehi@gmail.com>
2024-05-08 23:11:44 +0200

    Remove unused code.

--- source/gui/menu/log_menu.cpp
+++ source/gui/menu/log_menu.cpp
@@ -1,7 +1,7 @@
 #include "log_menu.h"
+
 namespace Tyr::Gui
 {
-
 void LogMenu::drawTerminal(const Common::Debug::Wrapper &t_wrapper)
 {
     const char *filter_choices[] = {"Trace - 0", "Debug - 1", "Info - 2", "Warning - 3", "Error - 4", "Critical - 5"};
--- source/gui/menu/log_menu.h
+++ source/gui/menu/log_menu.h
@@ -1,11 +1,6 @@
 #pragma once
 namespace Tyr::Gui
 {
-const std::map<Tyr::Common::Debug::Log::Level, std::string> log_level_map{
-    {Tyr::Common::Debug::Log::Level::Critical, "critical"}, {Tyr::Common::Debug::Log::Level::Trace, "trace"},
-    {Tyr::Common::Debug::Log::Level::Debug, "debug"},       {Tyr::Common::Debug::Log::Level::Info, "info"},
-    {Tyr::Common::Debug::Log::Level::Warning, "warning"},   {Tyr::Common::Debug::Log::Level::Error, "error"},
-};
 class LogMenu
 {
 private:
