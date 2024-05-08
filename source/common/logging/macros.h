18bca10bbaae8199ff947c837846376853a7eefa
Ali Salehi <nysalehi@gmail.com>
2024-05-08 21:09:21 +0200

    Force inline the log function.

--- source/common/logging/macros.h
+++ source/common/logging/macros.h
@@ -2,6 +2,14 @@
 
 #include "logging.h"
 
+#if defined(__clang__) || defined(__GNUC__)
+#define FORCEINLINE __attribute__((always_inline))
+#elif defined(_MSC_VER)
+#define FORCEINLINE __forceinline
+#else
+#define FORCEINLINE
+#endif
+
 namespace Tyr::Common
 {
 
@@ -9,8 +17,8 @@ namespace Tyr::Common
     template <typename... Args>                                                                                        \
     struct fn                                                                                                          \
     {                                                                                                                  \
-        fn(spdlog::format_string_t<Args...> format, Args &&...args,                                                    \
-           std::source_location             source = std::source_location::current())                                  \
+        FORCEINLINE fn(spdlog::format_string_t<Args...> format, Args &&...args,                                        \
+                       std::source_location             source = std::source_location::current())                      \
         {                                                                                                              \
             logger().log(source, spdlog::level::lvl, format, std::forward<Args>(args)...);                             \
         }                                                                                                              \
