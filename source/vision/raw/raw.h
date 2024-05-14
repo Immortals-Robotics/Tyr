f08d7f06bddcd828a42444c9d80962ff1dcaa328
Ali Salehi <nysalehi@gmail.com>
2024-05-14 19:44:55 +0200

    Use std array for m_packet_received.

--- source/vision/raw/raw.h
+++ source/vision/raw/raw.h
@@ -10,9 +10,9 @@ public:
 
     bool receive();
     void process();
-    bool camsReady() const;
-
     bool publish() const;
+
+    [[nodiscard]] bool camsReady() const;
 
     void updateAddress(const Common::NetworkAddress &t_address);
 
@@ -33,7 +33,8 @@ private:
 
     Common::RawBallState m_last_raw_ball; // The last position of the locked ball
 
-    bool                       m_packet_received[Common::Setting::kCamCount];
+    std::array<bool, Common::Setting::kCamCount> m_packet_received;
+
     Protos::SSL_DetectionFrame m_d_frame[Common::Setting::kCamCount];
 
     Common::RawWorldState m_state;
