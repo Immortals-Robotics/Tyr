e2f5c8416abdf58a37069a25f62a7b650ea4d9a5
Mohammad Shirazi <mhmmdshirazi@gmail.com>
2024-05-17 23:46:43 +0200

    Add info draw for drawing Ref state and fps

--- source/gui/renderer/info.cpp
+++ source/gui/renderer/info.cpp
@@ -1,3 +1,18 @@
-//
-// Created by Mohammad Shirazi on 17/05/2024.
-//
+#include "renderer.h"
+namespace Tyr::Gui
+{
+void Renderer::draw(const Common::RefereeState &t_referee_state, const Common::FieldState &t_field)
+{
+    Common::Vec2 top_left_corner =
+        Common::Vec2(t_field.width * -1 - 2 * t_field.boundary_width, t_field.height + 2 * t_field.boundary_width);
+
+    draw(top_left_corner + Common::Vec2(t_field.width * 2 + t_field.boundary_width, -150.),
+         "GUI FPS: " + std::to_string(GetFPS()), Common::Color::black(), 200);
+
+    auto ref_state = "Ref: " + t_referee_state.getString();
+    auto textSize  = MeasureTextEx(m_font, ref_state.c_str(), 400, 0).x;
+
+    draw(top_left_corner + Common::Vec2(150. + textSize / 2 , -150.), ref_state, Common::Color::red(), 400);
+}
+
+} // namespace Tyr::Gui
