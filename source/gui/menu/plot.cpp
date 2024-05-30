82bec5ebc36eb0627f2aac18b9583502fb66be1d
Ali Salehi <nysalehi@gmail.com>
2024-05-31 01:14:28 +0200

    Keep entire world states in plot history.

--- source/gui/menu/plot.cpp
+++ source/gui/menu/plot.cpp
@@ -12,88 +12,55 @@ PlotMenu::~PlotMenu()
     ImPlot::DestroyContext();
 }
 
-void PlotMenu::pushData(const Common::Vec2 &t_data, const Common::TimePoint &t_time, const bool &t_playback)
+void PlotMenu::pushData(const Common::WorldState &t_state, const bool &t_playback)
 {
-    if (m_last_time == t_time)
+    if (m_last_time == t_state.time)
     {
         return;
     }
 
-    if (std::abs((t_time - m_last_time).seconds()) > 0.2f)
+    if (std::abs((t_state.time - m_last_time).seconds()) > 0.2f)
     {
         m_data.clear();
     }
 
     if (m_data.size() >= kPlotQueueSize)
         m_data.pop_front();
-    m_data.emplace_back(t_data, t_time);
+    m_data.emplace_back(t_state);
 
-    m_last_time = t_time;
+    m_last_time = t_state.time;
 
     if (t_playback && m_data.size() > 1)
     {
         std::sort(m_data.begin(), m_data.end(),
-                  [](const Data &t_first, const Data &t_second) { return t_first.time < t_second.time; });
+                  [](const Common::WorldState &t_first, const Common::WorldState &t_second)
+                  { return t_first.time < t_second.time; });
     }
 }
 
 void PlotMenu::draw(const Common::WorldState &t_world, const bool &t_playback)
 {
-    switch (static_cast<Item>(m_item))
-    {
-    case Item::OurRobot:
-        for (const auto &robot : t_world.own_robot)
-        {
-            if (m_id == robot.vision_id)
-            {
-                pushData(robot.velocity, t_world.time, t_playback);
-            }
-        }
-        break;
-    case Item::OppRobot:
-        for (const auto &robot : t_world.opp_robot)
-        {
-            if (m_id == robot.vision_id)
-            {
-                pushData(robot.velocity, t_world.time, t_playback);
-            }
-        }
-        break;
-    case Item::Ball:
-        pushData(t_world.ball.velocity, t_world.time, t_playback);
-        break;
-    default:
-        break;
-    }
+    pushData(t_world, t_playback);
 
     if (ImGui::BeginTable("plot-setting", 3, ImGuiTableFlags_BordersInnerV))
     {
         const char *item_choices[] = {"Our Robot", "Opp Robot", "Ball"};
 
         ImGui::TableNextColumn();
-        if (ImGui::Combo("Item", reinterpret_cast<int *>(&m_item), item_choices, IM_ARRAYSIZE(item_choices)))
-        {
-            m_data.clear();
-        }
+        ImGui::Combo("Item", reinterpret_cast<int *>(&m_item), item_choices, IM_ARRAYSIZE(item_choices));
 
         if (m_item == Item::OurRobot || m_item == Item::OppRobot)
         {
             const char *id_choices[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 
             ImGui::TableNextColumn();
-            if (ImGui::Combo("ID", &m_id, id_choices, IM_ARRAYSIZE(id_choices)))
-            {
-                m_data.clear();
-            }
+            ImGui::Combo("ID", &m_id, id_choices, IM_ARRAYSIZE(id_choices));
         }
 
         const char *data_choices[] = {"vel", "vel xy"};
 
         ImGui::TableNextColumn();
-        if (ImGui::Combo("Data", reinterpret_cast<int *>(&m_type), data_choices, IM_ARRAYSIZE(data_choices)))
-        {
-            m_data.clear();
-        }
+        ImGui::Combo("Data", reinterpret_cast<int *>(&m_type), data_choices, IM_ARRAYSIZE(data_choices));
 
         ImGui::EndTable();
     }
@@ -104,29 +71,26 @@ void PlotMenu::draw(const Common::WorldState &t_world, const bool &t_playback)
     {
         auto getter_x = [](const int t_idx, void *t_user_data)
         {
-            const PlotMenu &menu = *(PlotMenu *) t_user_data;
-            const Data     &data = menu.m_data[t_idx];
+            const PlotMenu &menu = *static_cast<PlotMenu *>(t_user_data);
 
-            const float time = (data.time - menu.m_data.at(0).time).seconds();
-            return ImPlotPoint(time, data.data.x);
+            const auto [time, data] = menu.data(t_idx);
+            return ImPlotPoint(time.seconds(), data.x);
         };
 
         auto getter_y = [](const int t_idx, void *t_user_data)
         {
-            const PlotMenu &menu = *(PlotMenu *) t_user_data;
-            const Data     &data = menu.m_data[t_idx];
+            const PlotMenu &menu = *static_cast<PlotMenu *>(t_user_data);
 
-            const float time = (data.time - menu.m_data.at(0).time).seconds();
-            return ImPlotPoint(time, data.data.y);
+            const auto [time, data] = menu.data(t_idx);
+            return ImPlotPoint(time.seconds(), data.y);
         };
 
         auto getter_len = [](const int t_idx, void *t_user_data)
         {
-            const PlotMenu &menu = *(PlotMenu *) t_user_data;
-            const Data     &data = menu.m_data[t_idx];
+            const PlotMenu &menu = *static_cast<PlotMenu *>(t_user_data);
 
-            const float time = (data.time - menu.m_data.at(0).time).seconds();
-            return ImPlotPoint(time, data.data.length());
+            const auto [time, data] = menu.data(t_idx);
+            return ImPlotPoint(time.seconds(), data.length());
         };
 
         ImPlot::SetupAxes("time (s)", "Vel (mm/s)", ImPlotAxisFlags_None, ImPlotAxisFlags_AutoFit);
--- source/gui/menu/plot.h
+++ source/gui/menu/plot.h
@@ -29,14 +29,29 @@ private:
         Ball     = 2,
     };
 
-    struct Data
-    {
-        Data(const Common::Vec2 &t_vec, const Common::TimePoint &t_time) : data(t_vec), time(t_time)
-        {}
+    void pushData(const Common::WorldState &t_state, const bool &t_playback);
 
-        Common::Vec2      data;
-        Common::TimePoint time;
-    };
+    std::pair<Common::Duration, Common::Vec2> data(const int t_idx) const
+    {
+        const Common::WorldState &state = m_data[t_idx];
+        Common::Vec2              data{};
+
+        switch (m_item)
+        {
+        case Item::OurRobot:
+            data = state.own_robot[m_id].velocity;
+            break;
+        case Item::OppRobot:
+            data = state.opp_robot[m_id].velocity;
+            break;
+        case Item::Ball:
+            data = state.ball.velocity;
+            break;
+        }
+
+        const Common::Duration time = state.time - m_data.at(0).time;
+        return {time, data};
+    }
 
     static constexpr int kPlotQueueSize = 1000;
 
@@ -46,8 +61,6 @@ private:
 
     Common::TimePoint m_last_time;
 
-    std::deque<Data> m_data;
-
-    void pushData(const Common::Vec2 &t_data, const Common::TimePoint &t_time, const bool &t_playback);
+    std::deque<Common::WorldState> m_data;
 };
 } // namespace Tyr::Gui
