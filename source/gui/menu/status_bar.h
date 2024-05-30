70070a05105558b085dc84c7eb209d36b585c610
Ali Salehi <nysalehi@gmail.com>
2024-05-31 01:15:46 +0200

    Minor cleanup.

--- source/gui/menu/status_bar.h
+++ source/gui/menu/status_bar.h
@@ -8,7 +8,8 @@ public:
     void draw(const Common::Debug::Wrapper &t_wrapper);
 
 private:
-    std::unordered_map<std::string, Common::MedianFilter<Common::Duration, 30>> m_interval_filters;
-    std::unordered_map<std::string, Common::MedianFilter<Common::Duration, 30>> m_duration_filters;
+    using Filter = Common::MedianFilter<Common::Duration, 30>;
+    std::unordered_map<std::string, Filter> m_interval_filters;
+    std::unordered_map<std::string, Filter> m_duration_filters;
 };
 } // namespace Tyr::Gui
