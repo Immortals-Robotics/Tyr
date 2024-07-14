1d000768df34ec0642a805098e10033dcc571fc9
Ali Salehi <nysalehi@gmail.com>
2024-07-14 10:56:23 +0200

    Tuned the attack switch cost function.

--- source/soccer/helpers/manage_att_roles.cpp
+++ source/soccer/helpers/manage_att_roles.cpp
@@ -2,25 +2,31 @@
 
 namespace Tyr::Soccer
 {
-static int           attackerChangeHys = 0;
-static constexpr int kRequiredHys      = 30;
+static int             attackerChangeHys = 0;
+static constexpr float kChangeHysSeconds = 0.5f;
 
 void Ai::manageAttRoles()
 {
+    const int required_hys_frames = kChangeHysSeconds * Common::config().vision.vision_frame_rate;
+
     int *best_attack =
         *std::max_element(ids.begin(), ids.end(), [&](int *lhs, int *rhs)
                           { return calculateSwitchToAttackerScore(*lhs) < calculateSwitchToAttackerScore(*rhs); });
 
-    if (calculateSwitchToAttackerScore(m_attack) < 0.f)
+    const float current_attack_score = calculateSwitchToAttackerScore(m_attack);
+    const float best_attack_score    = calculateSwitchToAttackerScore(*best_attack);
+
+    // < 0 means the current robot should be switched immediately, ie. out of the game
+    if (current_attack_score < 0.f && best_attack_score >= 0.0f)
     {
-        attackerChangeHys = kRequiredHys + 1;
+        attackerChangeHys = required_hys_frames + 1;
     }
-    else if (*best_attack != m_attack)
+    else if (*best_attack != m_attack && best_attack_score - current_attack_score > 0.1f)
     {
         attackerChangeHys++;
     }
 
-    if (attackerChangeHys > kRequiredHys)
+    if (attackerChangeHys > required_hys_frames)
     {
         std::swap(m_attack, *best_attack);
         attackerChangeHys = 0;
