306d604dcb03f5e6be10e2da69c4b82c15f42b84
Ali Salehi <nysalehi@gmail.com>
2025-03-10 15:48:57 +0100

    Remove unneeded comments.

--- source/vision/kalman/control.h
+++ source/vision/kalman/control.h
@@ -2,23 +2,13 @@
 
 namespace Tyr::Vision::Filter
 {
-/**
- * @brief System control-input vector-type for an omnidirectional 3DOF planar robot
- *
- * The control inputs are now independent velocities in x and y directions
- * and the change in orientation (`dtheta`).
- *
- */
 class Control : public Kalman::Vector<float, 3>
 {
 public:
     KALMAN_VECTOR(Control, float, 3)
 
-    //! Velocity in X direction
     static constexpr size_t VX = 0;
-    //! Velocity in Y direction
     static constexpr size_t VY = 1;
-    //! Angular Rate (Orientation change)
     static constexpr size_t DTHETA = 2;
 
     float vx()      const { return (*this)[ VX ]; }
