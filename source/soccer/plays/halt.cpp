e7441f7d9c117dd1f9fa9eff769d23d5fd9a3cd3
Ali Salehi <nysalehi@gmail.com>
2025-03-14 15:54:46 +0100

    Break in halt.

--- source/soccer/plays/halt.cpp
+++ source/soccer/plays/halt.cpp
@@ -4,7 +4,20 @@ namespace Tyr::Soccer
 {
 void Ai::halt(const int t_robot_num)
 {
-    m_own_robot[t_robot_num].halt();
+    const Common::Vec2 last_cmd = m_own_robot[t_robot_num].currentMotion();
+
+    if (last_cmd.length() > 100.0f)
+    {
+        const VelocityProfile profile = VelocityProfile::mamooli();
+        const float dt = 1.0f / Common::config().vision.vision_frame_rate;
+        const Common::Vec2 new_cmd = last_cmd - last_cmd.normalized() * profile.acceleration * dt;
+
+        m_own_robot[t_robot_num].move(new_cmd);
+    }
+    else
+    {
+        m_own_robot[t_robot_num].halt();
+    }
 }
 
 void Ai::haltAll()
